7fe164b1776518504e49fd2f9c4bd927cd1c3f63
Marcos Suarez <markozsuarez01@gmail.com>
2026-03-07 20:42:34 -0500

    01: implement first-person camera mode

--- 01_car_controller/Assets/Scripts/FollowPlayer.cs
+++ 01_car_controller/Assets/Scripts/FollowPlayer.cs
@@ -5,19 +5,50 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
 
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -10);
+    public Transform player;
+    private Vector3 thirdPersonOffset = new Vector3(0, 5, -10);
+
+    private Vector3 firstPersonOffset = new Vector3(0, 3.12f, -0.5f);
+
+    private bool isThirdPerson = true;
+
+    private Quaternion thirdPersonInitialRotation;
+
+    private float horizontalInput;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        thirdPersonInitialRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        //Toggle between first-person and third-person view
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            isThirdPerson = !isThirdPerson;
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+
+        //Get horizontal input for camera rotation
+        horizontalInput = Input.GetAxis("Horizontal");
+
         //Follow the player's position
-        transform.position = player.transform.position + offset;
+        if(isThirdPerson)
+        {
+            transform.position = player.transform.position + thirdPersonOffset;
+            transform.rotation = thirdPersonInitialRotation;
+        }else
+        {
+            transform.position = player.position + player.rotation * firstPersonOffset;
+            transform .rotation = player.rotation;
+        }
+        
     }
 }
