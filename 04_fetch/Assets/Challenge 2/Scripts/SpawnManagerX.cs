d27e6acd9e2ab75ba60da625690f71d0c4f15759
Marcos Suarez <markozsuarez01@gmail.com>
2026-03-16 12:44:45 -0400

    Initialize 04_fetch Unity project with Challenge 2 scene and assets

---
+++ 04_fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    // Array of ball prefabs
+    [SerializeField] GameObject[] ballPrefabs;
+
+    // Spawn position limits
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    // Spawn timing
+    private float startDelay = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        // Generate random ball index
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // Instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        // Random spawn time
+        float spawnTime = Random.Range(3,5);
+
+        // Invoke SpawnRandomBall again after a random delay
+        Invoke("SpawnRandomBall", spawnTime);
+
+    }
+
+}
