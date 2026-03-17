e4db11b022894a96c6f16bdc03f4ce9e1173e295
Marcos Suarez <markozsuarez01@gmail.com>
2026-03-17 06:19:01 -0400

    Limit player spawning to once per second

--- 04_fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ 04_fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public GameObject dogPrefab;
+    [SerializeField]public GameObject dogPrefab;
+
+    private float nextSpawnTime;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawnTime)
         {
+            // Set the next spawn time to be 1 second from now
+            nextSpawnTime = Time.time + 1.0f;
+            
+            // Instantiate the dog prefab at the player's position and rotation
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
         }
     }
+
+
 }
