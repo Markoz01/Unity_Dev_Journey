f1cd793eec1d245c06aded04fa1619da120123a2
Marcos Suarez <markozsuarez01@gmail.com>
2026-05-21 19:48:56 -0400

    Add repeating background and obstacle spawner

--- 06_RunningInTheFarm/Assets/Scripts/SpawnManager.cs
+++ 06_RunningInTheFarm/Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,22 @@ public class SpawnManager : MonoBehaviour
 
     [SerializeField] private GameObject obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
 
     void Start()
     {
-        Instantiate(obstaclePrefabs, spawnPos, obstaclePrefabs.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     void Update()
     {
         
     }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefabs, spawnPos, obstaclePrefabs.transform.rotation);
+    }
+
 }
