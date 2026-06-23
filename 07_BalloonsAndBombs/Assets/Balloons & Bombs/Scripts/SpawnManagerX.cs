9f36393562a63b8657ffb4a1fd7f0f3ccd6968eb
Marcos Suarez <markozsuarez01@gmail.com>
2026-06-23 19:39:52 -0400

    Fix: correct object spawning in ManagerSpawn

--- 07_BalloonsAndBombs/Assets/Balloons & Bombs/Scripts/SpawnManagerX.cs
+++ 07_BalloonsAndBombs/Assets/Balloons & Bombs/Scripts/SpawnManagerX.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -10,10 +8,9 @@ public class SpawnManagerX : MonoBehaviour
 
     private PlayerControllerX playerControllerScript;
 
-    // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
